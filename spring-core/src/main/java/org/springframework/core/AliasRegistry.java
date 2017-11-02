f7a174ae16236c9cbc67c3c17f9ceb49c7536258
meihuai.liang <meihuai.liang@tendcloud.com>
2017-11-02 11:03:45 +0800

    Alias管理别名的公共接口，作为BeanDefinitionRegistry的父类接口

--- spring-core/src/main/java/org/springframework/core/AliasRegistry.java
+++ spring-core/src/main/java/org/springframework/core/AliasRegistry.java
@@ -19,7 +19,7 @@ package org.springframework.core;
 /**
  * Common interface for managing aliases. Serves as super-interface for
  * {@link org.springframework.beans.factory.support.BeanDefinitionRegistry}.
- *
+ *管理别名的公共接口，作为BeanDefinitionRegistry的父类接口
  * @author Juergen Hoeller
  * @since 2.5.2
  */
@@ -27,17 +27,21 @@ public interface AliasRegistry {
 
 	/**
 	 * Given a name, register an alias for it.
-	 * @param name the canonical name
-	 * @param alias the alias to be registered
+	 * 为一个name注册一个别名
+	 * @param name the canonical name 标准名称
+	 * @param alias the alias to be registered 被注册的别名
 	 * @throws IllegalStateException if the alias is already in use
 	 * and may not be overridden
+	 * 抛出IllegalStateException 如果这个别名已经被使用且不能够被覆盖
 	 */
 	void registerAlias(String name, String alias);
 
 	/**
 	 * Remove the specified alias from this registry.
-	 * @param alias the alias to remove
+	 * 从注册的地方移除掉特定的别名
+	 * @param alias the alias to remove 被移除的别名
 	 * @throws IllegalStateException if no such alias was found
+	 * 抛出IllegalStateException异常，如果查找不大这个别名
 	 */
 	void removeAlias(String alias);
 
@@ -46,6 +50,10 @@ public interface AliasRegistry {
 	 * (as opposed to the name of an actually registered component).
 	 * @param beanName the bean name to check
 	 * @return whether the given name is an alias
+	 * 确定这个给定的名称是否定义为别名
+	 * (与实际上注册的组件的名称相反)
+	 * 要检查的bean名称
+	 * 返回：给定的名称是否为别名
 	 */
 	boolean isAlias(String beanName);
 
@@ -53,6 +61,9 @@ public interface AliasRegistry {
 	 * Return the aliases for the given name, if defined.
 	 * @param name the name to check for aliases
 	 * @return the aliases, or an empty array if none
+	 * 返回给定名称的别名，如果这个别名已经被定义
+	 * 检查别名的名称
+	 * 返回:别名，或空数组，如果没有
 	 */
 	String[] getAliases(String name);
 
